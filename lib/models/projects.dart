74a643be35346769782bc43b4792079aed134f62
Jonathan Lauxen Romano <jjonathanromano@gmail.com>
2023-10-23 14:08:26 +0100

    added instructions to project model

--- lib/models/projects.dart
+++ lib/models/projects.dart
@@ -3,12 +3,14 @@ class Project {
   final String name;
   final String reference;
   final Status status;
+  final Instruction? instructions;
 
   const Project({
     required this.id,
     required this.name,
     required this.reference,
     required this.status,
+    this.instructions,
   });
 
   factory Project.fromJson(Map<String, dynamic> json) => Project(
@@ -24,6 +26,20 @@ class Project {
         "reference": reference,
         "status": statusValues.reverse[status],
       };
+}
+
+class Instruction {
+  final String sourceImageFileName;
+  final String instructionsImageFileName;
+
+  const Instruction({
+    required this.sourceImageFileName,
+    required this.instructionsImageFileName,
+  });
+
+  factory Instruction.fromJson(Map<String, dynamic> json) => Instruction(
+      sourceImageFileName: json["source_filename"],
+      instructionsImageFileName: json["instructions_filename"]);
 }
 
 enum Status { created, downloaded, ongoing, open, underVerification }
